54d4f5da8b2023b977d52f691dd00aa2caecc8d4
carl <karol-nowakowski@o2.pl>
2021-01-01 17:33:55 +0100

    Create library-list aplication

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {ApolloClient, InMemoryCache, HttpLink, ApolloProvider} from "@apollo/client";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const URL = "https://library-applicationn.herokuapp.com/";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache({
+    addTypename: false,
+    resultCaching: false
+  }),
+  link: new HttpLink({
+    uri: URL
+  }),
+  queryDeduplication: false
+});
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
