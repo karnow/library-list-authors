3cbeda78275766ad8c4d673cd5544ae07dbf3f35
carl <karol-nowakowski@o2.pl>
2021-02-15 18:15:21 +0100

    Create ReturnBookCopy mutation ReturnButton.js

--- src/components/BookCopy/ReturnButton.js
+++ src/components/BookCopy/ReturnButton.js
@@ -1,9 +1,22 @@
+import { gql, useMutation } from "@apollo/client";
 import { Button } from "@chakra-ui/react";
 import React from "react";
+import { BOOK_COPY_FIELDS_FRAGMENT } from './fragments';
 
+const RETURN_BOOK_COPY_MUTATION = gql`
+mutation ReturnBookCopy($bookCopyId: ID!) {
+  returnBookCopy(id: $bookCopyId) {
+    id
+    ...bookCopyFields
+  }
+}
+${BOOK_COPY_FIELDS_FRAGMENT}
+`;
 export default function BorrowButton({ borrowedBookCopy }) {
+
+  const [returnBookcopy, {loading}]= useMutation(RETURN_BOOK_COPY_MUTATION, {variables: {bookCopyId: borrowedBookCopy.id}})
   return (
-    <Button onClick={() => console.log("User wants to return the book copy")}>
+    <Button disabled={loading} onClick={returnBookcopy}>
       Return
     </Button>
   );
