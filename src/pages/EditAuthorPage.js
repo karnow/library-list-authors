5e785338f26c1fe8e1add8f676a5be06236a99b0
carl <karol-nowakowski@o2.pl>
2021-03-10 14:38:34 +0100

    Create UpdateAuthorMutation in EditAuthorPage.js

--- src/pages/EditAuthorPage.js
+++ src/pages/EditAuthorPage.js
@@ -1,9 +1,24 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 import React from "react";
 import { useNavigate, useParams } from "react-router";
 import { useToast } from "../components/Toast";
 import AuthorUpdateForm from "../components/AuthorUpdateForm";
 import { GET_AUTHOR_QUERY } from "./AuthorDetailsPage";
+import {AUTHOR_DETAILS_FIRLEDS_FRAGMENT} from "../components/AuthorDetails";
+
+
+const UPDATE_AUTHOR_MUTATION = gql`
+mutation UpdateAuthor($updateAuthorInput: UpdateAuthorInput!){
+  updateAuthor(input: $updateAuthorInput){
+    success
+    message
+    author {
+      ...authorDetailFields
+    }
+  }
+}
+${AUTHOR_DETAILS_FIRLEDS_FRAGMENT}
+`;
 
 export default function EditAuthorPage() {
     const toast = useToast();
@@ -11,6 +26,18 @@ export default function EditAuthorPage() {
     const {authorId } = useParams();
     const { loading, error, data } = useQuery(GET_AUTHOR_QUERY, {
         variables: { authorId }
+    });
+
+    const [updateAuthor, { loading: isUpdating }] = useMutation(UPDATE_AUTHOR_MUTATION, {
+        onCompleted: ({ updateAuthor: { success, message } }) => {
+            toast({
+                description: message,
+                status: success ? "success" : "error"
+            });
+            if (success) {
+                navigate(`/author/${authorId}`);
+            }
+        }
     });
     if (loading) {
         return <p>Loading...</p>;
@@ -26,8 +53,6 @@ export default function EditAuthorPage() {
   
     return <AuthorUpdateForm
         author={author}
-        onUpdate={() => {
-            toast({ status: "warning", description: "NOT IMPLEMENTED" })
-        }} isUpdating={false}
+        onUpdate={(updateAuthorInput) => {updateAuthor({variables: {updateAuthorInput}})}} isUpdating={isUpdating}
         onCancel={() => navigate(`/author/${author.id}`)} />
 }
