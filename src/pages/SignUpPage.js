35389f2dfd2fa779b9dc0b82f1bafcb062e15a9c
carl <karol-nowakowski@o2.pl>
2021-03-30 14:10:09 +0200

    Create SignUp mutation in SignUpPage.js

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,11 +1,38 @@
 import React from "react";
 import SignUpForm from "../components/SignUpForm";
+import { gql, useMutation } from "@apollo/client";
+import { useToast } from "../components/Toast";
+import { useAuth } from "../components/AuthProvider";
 
+const SIGN_UP_MUTATION = gql`
+mutation SignUp($signUpData: SignInInput!){
+  signUp(input: $signUpData) {
+    success
+    message
+    token
+    
+  }
+}
+`;
 export default function SignUpPage() {
+   const { authorize } = useAuth();
+  const toast = useToast();
+  const [signUp, { loading }] = useMutation(SIGN_UP_MUTATION, {
+    onCompleted: ({ signUp: { success, message, token } }) => {
+      toast({
+        description: message,
+        status: success ? "success" : "error"
+      })
+      if (success) {
+        authorize(token);
+      }
+    }
+  });
+ 
   return (
     <SignUpForm
-      onSignUp={signUpData => console.log("Trying to sign up as", signUpData)}
-      isSigningUp={false}
+      onSignUp={signUpData => signUp({variables:{signUpData}})}
+      isSigningUp={loading}
     />
   );
 }
