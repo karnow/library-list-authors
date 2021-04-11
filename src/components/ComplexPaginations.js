46ea5cc4629cceb1a0418e96fe6f853d499e294f
carl <karol-nowakowski@o2.pl>
2021-04-11 12:15:11 +0200

    Create ClosestPageButtons components

--- src/components/ComplexPaginations.js
+++ src/components/ComplexPaginations.js
@@ -1,25 +1,19 @@
 import { Flex } from "@chakra-ui/react";
 import React from "react";
 import PageButton from "./Pagination/PageButton";
+import ClosestPagesButtons from "../components/Pagination/ClosestPagesButtons";
 
 export default function ComplexPagination({ pageInfo, onPageChange }) {
-  const { currentPageNumber, previousPageNumber, nextPageNumber, firstNumberPage, lastPageNumber } = pageInfo;
+  const { currentPageNumber, firstNumberPage, lastPageNumber } = pageInfo;
     const commonPageButtonProps = { currentPageNumber, onPageChange };
   return (
     <Flex justifyContent="space-between" my="5">
       <PageButton newPageNumber={firstNumberPage} {...commonPageButtonProps}>
         First Page
       </PageButton>
-      <PageButton newPageNumber={previousPageNumber} {...commonPageButtonProps}>
-        Previous Page
-      </PageButton>
-      <PageButton
-        newPageNumber={currentPageNumber}
-        {...commonPageButtonProps}
-      />
-      <PageButton newPageNumber={nextPageNumber} {...commonPageButtonProps}>
-        Next Page
-      </PageButton>
+      
+      <ClosestPagesButtons pageInfo={pageInfo} onPageChange={onPageChange}/>
+
       <PageButton newPageNumber={lastPageNumber} {...commonPageButtonProps}>
         Last Page
       </PageButton>
